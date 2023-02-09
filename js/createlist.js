d3b8cb9abacb1f4e1d410f1aa9cbbcff9a872fb3
Nguyễn Thái Nguyên <thnguyen.7620@gmail.com>
2023-02-09 16:23:26 +0700

    push project

---
+++ js/createlist.js
@@ -0,0 +1,302 @@
+var dsdt=new Array();
+var dt={
+    nb:true,
+    chon:true,
+    loai:'iphone',
+    link:'../html/Iphone12ProMax.html',
+    imga:'<img src="../image/iphone/iphone-12-pro-max-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'iPhone 12 Pro Max 512GB',
+    gia:'41490000',
+    giacu:'42990000',
+    manhinh:'Màn hình 6.7"',
+    chip:'Chip Apple A14 Bionic',
+    ram:'RAM 6 GB, ROM 512 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'iphone',
+    link:'../html/Iphone12.html',
+    imga:'<img src="../image/iphone/iphone-12bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'iPhone 12 256GB',
+    gia:'25490000',
+    giacu:'26490000',
+    manhinh:'Màn hình 6.1"',
+    chip:'Chip Apple A14 Bionic',
+    ram:'RAM 4 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'iphone',
+    link:'../html/Iphone12Mini.html',
+    imga:'<img src="../image/iphone/iphone-23-mini-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'iPhone 12 mini 256GB',
+    gia:'21490000',
+    giacu:'22490000',
+    manhinh:'Màn hình 5.4"',
+    chip:'Chip Apple A14 Bionic',
+    ram:'RAM 4 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'iphone',
+    link:'../html/IphoneXr.html',
+    imga:'<img src="../image/iphone/iphone-xr-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'iPhone XR 128GB',
+    gia:'15490000',
+    giacu:'16490000',
+    manhinh:'Màn hình 6.1"',
+    chip:'Chip Apple A12 Bionic',
+    ram:'RAM 3 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'samsung',
+    link:'../html/SamsungZFold2.html',
+    imga:'<img src="../image/samsung/samsung-galaxy-z-fold-2-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Samsung Galaxy Z Fold2 5G',
+    gia:'50000000',
+    giacu:'50500000',
+    manhinh:'Màn hình Chính 7.59" & Phụ 6.23""',
+    chip:'Chip Snapdragon 865+',
+    ram:'RAM 12 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'samsung',
+    link:'../html/SamsungNote20U.html',
+    imga:'<img src="../image/samsung/samsung-galaxy-note-20-ultra-bia0.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Samsung Galaxy Note 20 Ultra 5G',
+    gia:'23990000',
+    giacu:'32990000',
+    manhinh:'Màn hình 6.9"',
+    chip:'Chip Exynos 990',
+    ram:'RAM 12 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'samsung',
+    link:'../html/SamsungS21U.html',
+    imga:'<img src="../image/samsung/samsung-galaxy-s21-ultra-bia0.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Samsung Galaxy S21 Ultra 5G',
+    gia:'28990000',
+    giacu:'33990000',
+    manhinh:'Màn hình 6.8"',
+    chip:'Chip Exynos 2100',
+    ram:'RAM 12 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'samsung',
+    link:'../html/SamsungA72.html',
+    imga:'<img src="../image/samsung/samsung-galaxy-a72bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Samsung Galaxy A72',
+    gia:'11190000',
+    giacu:'11490000',
+    manhinh:'Màn hình 6.7"',
+    chip:'Chip Snapdragon 720G',
+    ram:'RAM 8 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'xiaomi',
+    link:'../html/XiaomiMi11.html',
+    imga:'<img src="../image/xiaomi/xiaomi-mi-11-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Xiaomi Mi 11 5G',
+    gia:'20990000',
+    giacu:'21990000',
+    manhinh:'Màn hình 6.81"',
+    chip:'Chip Snapdragon 888',
+    ram:'RAM 8 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'xiaomi',
+    link:'../html/XiaomiRN10P.html',
+    imga:'<img src="../image/xiaomi/xiaomi-redmi-note-10-pro-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Xiaomi Redmi Note 10 Pro',
+    gia:'7490000',
+    giacu:'7600000',
+    manhinh:'Màn hình 6.67"',
+    chip:'Chip Snapdragon 732G',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'xiaomi',
+    link:'../html/XiaomiPX3N.html',
+    imga:'<img src="../image/xiaomi/xiaomi-poco-x3-nfc-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Xiaomi POCO X3 NFC',
+    gia:'5990000',
+    giacu:'6990000',
+    manhinh:'Màn hình 6.67"',
+    chip:'Chip Snapdragon 732G',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'xiaomi',
+    link:'../html/XiaomiRN8.html',
+    imga:'<img src="../image/xiaomi/xiaomi-redmi-note-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Xiaomi Redmi Note 8',
+    gia:'4290000',
+    giacu:'4490000',
+    manhinh:'Màn hình 6.3"',
+    chip:'Chip Snapdragon 665',
+    ram:'RAM 4 GB, ROM 64 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'oppo',
+    link:'../html/OppoR5.html',
+    imga:'<img src="../image/oppo/oppo-reno5-5g-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'OPPO Reno5 5G',
+    gia:'10990000',
+    giacu:'11990000',
+    manhinh:'Màn hình 6.43"',
+    chip:'Chip Snapdragon 765G',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'oppo',
+    link:'../html/OppoR4P.html',
+    imga:'<img src="../image/oppo/oppo-reno4-pro-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'OPPO Reno4 Pro',
+    gia:'10490000',
+    giacu:'10690000',
+    manhinh:'Màn hình 6.5"',
+    chip:'Chip Snapdragon 720G',
+    ram:'RAM 8 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'oppo',
+    link:'../html/OppoFX3P.html',
+    imga:'<img src="../image/oppo/oppo-find-x3-pro-black-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'OPPO Find X3 Pro 5G',
+    gia:'26990000',
+    giacu:'27490000',
+    manhinh:'Màn hình 6.7"',
+    chip:'Chip Snapdragon 888',
+    ram:'RAM 12 GB, ROM 256 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'oppo',
+    link:'../html/OppoA94.html',
+    imga:'<img src="../image/oppo/oppo-a94-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'OPPO A94',
+    gia:'7690000',
+    giacu:'7990000',
+    manhinh:'Màn hình 6.43"',
+    chip:'Chip MediaTek Helio P95',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:true,
+    chon:true,
+    loai:'vivo',
+    link:'../html/VivoV21.html',
+    imga:'<img src="../image/vivo/vivo-v21-5g-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Vivo V21 5G',
+    gia:'9690000',
+    giacu:'9990000',
+    manhinh:'Màn hình 6.44"',
+    chip:'Chip MediaTek Dimensity 800U 5G',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'vivo',
+    link:'../html/VivoY72.html',
+    imga:'<img src="../image/vivo/vivo-y72-5g-blue-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Vivo Y72 5G',
+    gia:'7690000',
+    giacu:'7990000',
+    manhinh:'Màn hình 6.58"',
+    chip:'Chip MediaTek Dimensity 700',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'vivo',
+    link:'../html/VivoV20.html',
+    imga:'<img src="../image/vivo/vivo-v20-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Vivo V20',
+    gia:'7790000',
+    giacu:'8490000',
+    manhinh:'Màn hình 6.44"',
+    chip:'Chip Snapdragon 720G',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var dt={
+    nb:false,
+    chon:true,
+    loai:'vivo',
+    link:'../html/VivoY51.html',
+    imga:'<img src="../image/vivo/vivo-y51-bia.jpg" alt="dt" style="max-width: 170px; height: auto;" id="hinh">',
+    ten:'Vivo Y51',
+    gia:'5790000',
+    giacu:'6290000',
+    manhinh:'Màn hình 6.58"',
+    chip:'Chip Snapdragon 662',
+    ram:'RAM 8 GB, ROM 128 GB',
+}
+dsdt.push(dt);
+var taikhoan=new Array();
+var tk={
+    tentk:'admin',
+    hoten:'Nguyễn Thái Nguyên_AD',
+    matkhau:'admin',
+    dt:'0339875293',
+}
+taikhoan.push(tk);
+var tk={
+    tentk:'user',
+    hoten:'Nguyễn Thái Nguyên',
+    matkhau:'user',
+    dt:'0339875293',
+}
+taikhoan.push(tk);
+var giohang=new Array();
+localStorage.setItem('giohang',JSON.stringify(giohang));
+localStorage.setItem('dsdt',JSON.stringify(dsdt));
+localStorage.setItem('tk',JSON.stringify(taikhoan));
+localStorage.setItem('ttdn','false');
+localStorage.setItem('tukhoa','iphone');
